dmaulikr/chatty

Step 1.7: Add graphqlExpress

server/index.js
@@ -1,7 +1,35 @@
 import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import bodyParser from 'body-parser';
+import { createServer } from 'http';
 
-const PORT = 8080;
+import { Schema } from './data/schema';
+import { Mocks } from './data/mocks';
 
+const GRAPHQL_PORT = 8080;
 const app = express();
 
-app.listen(PORT, () => console.log(`Server is now running on http://localhost:${PORT}`));
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+});
+
+addMockFunctionsToSchema({
+  schema: executableSchema,
+  mocks: Mocks,
+  preserveResolvers: true,
+});
+
+// `context` must be an object and can't be undefined when using connectors
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema: executableSchema,
+  context: {}, // at least(!) an empty object
+}));
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+
+const graphQLServer = createServer(app);
+
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`));

agiletechvn/ReactNativeGraphql

server/index.js
@@ -1,7 +1,35 @@
 import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import bodyParser from 'body-parser';
+import { createServer } from 'http';
 
-const PORT = 8080;
+import { Schema } from './data/schema';
+import { Mocks } from './data/mocks';
 
+const GRAPHQL_PORT = 8080;
 const app = express();
 
-app.listen(PORT, () => console.log(`Server is now running on http://localhost:${PORT}`));
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+});
+
+addMockFunctionsToSchema({
+  schema: executableSchema,
+  mocks: Mocks,
+  preserveResolvers: true,
+});
+
+// `context` must be an object and can't be undefined when using connectors
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema: executableSchema,
+  context: {}, // at least(!) an empty object
+}));
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+
+const graphQLServer = createServer(app);
+
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`));