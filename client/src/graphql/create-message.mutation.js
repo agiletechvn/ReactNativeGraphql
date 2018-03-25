dmaulikr/chatty

Step 8.5: Add Input Types to Mutations

client/src/graphql/create-message.mutation.js
@@ -3,8 +3,8 @@ import gql from 'graphql-tag';
 import MESSAGE_FRAGMENT from './message.fragment';
 
 const CREATE_MESSAGE_MUTATION = gql`
-  mutation createMessage($text: String!, $groupId: Int!) {
-    createMessage(text: $text, groupId: $groupId) {
+  mutation createMessage($message: MessageInput!) {
+    createMessage(message: $message) {
       ... MessageFragment
     }
   }

client/src/graphql/login.mutation.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 const LOGIN_MUTATION = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+  mutation login($user: UserInput!) {
+    login(user: $user) {
       id
       jwt
       username

client/src/graphql/signup.mutation.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 const SIGNUP_MUTATION = gql`
-  mutation signup($email: String!, $password: String!) {
-    signup(email: $email, password: $password) {
+  mutation signup($user: UserInput!) {
+    signup(user: $user) {
       id
       jwt
       username

agiletechvn/ReactNativeGraphql

client/src/graphql/create-message.mutation.js
@@ -3,8 +3,8 @@ import gql from 'graphql-tag';
 import MESSAGE_FRAGMENT from './message.fragment';
 
 const CREATE_MESSAGE_MUTATION = gql`
-  mutation createMessage($text: String!, $groupId: Int!) {
-    createMessage(text: $text, groupId: $groupId) {
+  mutation createMessage($message: CreateMessageInput!) {
+    createMessage(message: $message) {
       ... MessageFragment
     }
   }

client/src/graphql/login.mutation.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 const LOGIN_MUTATION = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+  mutation login($user: SigninUserInput!) {
+    login(user: $user) {
       id
       jwt
       username

client/src/graphql/signup.mutation.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 const SIGNUP_MUTATION = gql`
-  mutation signup($email: String!, $password: String!) {
-    signup(email: $email, password: $password) {
+  mutation signup($user: SigninUserInput!) {
+    signup(user: $user) {
       id
       jwt
       username