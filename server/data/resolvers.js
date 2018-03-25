dmaulikr/chatty

Step 8.4: Add Input Types to Resolvers and Logic

server/data/resolvers.js
@@ -46,8 +46,10 @@ export const Resolvers = {
     updateGroup(_, args, ctx) {
       return groupLogic.updateGroup(_, args, ctx);
     },
-    login(_, { email, password }, ctx) {
+    login(_, userInput, ctx) {
       // find user by email
+      const { email, password } = userInput.user;
+
       return User.findOne({ where: { email } }).then((user) => {
         if (user) {
           // validate password
@@ -71,7 +73,9 @@ export const Resolvers = {
         return Promise.reject('email not found');
       });
     },
-    signup(_, { email, password, username }, ctx) {
+    signup(_, userInput, ctx) {
+      const { email, password, username } = userInput.user;
+
       // find user by email
       return User.findOne({ where: { email } }).then((existing) => {
         if (!existing) {

agiletechvn/ReactNativeGraphql

server/data/resolvers.js
@@ -55,8 +55,10 @@ export const Resolvers = {
     updateGroup(_, args, ctx) {
       return groupLogic.updateGroup(_, args, ctx);
     },
-    login(_, { email, password }, ctx) {
+    login(_, signinUserInput, ctx) {
       // find user by email
+      const { email, password } = signinUserInput.user;
+
       return User.findOne({ where: { email } }).then((user) => {
         if (user) {
           // validate password
@@ -80,7 +82,9 @@ export const Resolvers = {
         return Promise.reject('email not found');
       });
     },
-    signup(_, { email, password, username }, ctx) {
+    signup(_, signinUserInput, ctx) {
+      const { email, password, username } = signinUserInput.user;
+
       // find user by email
       return User.findOne({ where: { email } }).then((existing) => {
         if (!existing) {